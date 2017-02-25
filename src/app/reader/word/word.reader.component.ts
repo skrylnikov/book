8809767d0bfcc837074ea3ec3d444bff2419aa82
worldxaker <djxakir714@gmail.com>
2017-02-25 23:11:04 +0300

    fix translate(no)

--- src/app/reader/word/word.reader.component.ts
+++ src/app/reader/word/word.reader.component.ts
@@ -2,9 +2,19 @@ import { Component, Input } from '@angular/core'
 
 @Component({
   selector:'word',
-  templateUrl:'./word.reader.component.html'
+  templateUrl:'./word.reader.component.html',
+  styleUrls:['./word.reader.component.css']
 })
 export class WordReadComponent{
   @Input() word:string;
+  isTranslate:boolean=false;
+
+  wordTranslate:string;
+
+  translate(){
+    this.isTranslate=true;
+    //TODO add yandex translate
+    this.wordTranslate=this.word;
+  }
 
 }
