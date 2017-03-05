5aa950f42e10905bf2225a4ae4e9dc6c2af7f762
worldxaker <djxakir714@gmail.com>
2017-03-05 17:16:12 +0300

    delete modal window

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,22 +1,9 @@
-import { Component, ViewContainerRef } from '@angular/core';
-import { Overlay } from 'angular2-modal';
-import { Modal } from 'angular2-modal/plugins/bootstrap';
+import { Component } from '@angular/core';
 
 @Component({
   selector:'header',
   templateUrl:'./header.component.html'
 })
 export class HeaderComponent{
-  constructor(overlay: Overlay, vcRef: ViewContainerRef, public modal: Modal) {
-    overlay.defaultViewContainer = vcRef;
-  }
 
-  onClick() {
-    this.modal.alert()
-      .size('lg')
-      .showClose(true)
-      .title('Login')
-      .body(`когда-то здесь будет логи`)
-      .open();
-  }
 }
