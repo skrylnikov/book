7c47e9ca51180d5923767981d3697389c9146d61
worldxaker <djxakir714@gmail.com>
2017-03-06 02:41:37 +0300

    create new modal window, and add variable translate using yandex dictionary

--- src/app/reader/word/word.reader.component.ts
+++ src/app/reader/word/word.reader.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'
+import {Component, Input, transition} from '@angular/core'
 
 import { TranslateService } from '../../shared/translate.service'
 
@@ -11,14 +11,41 @@ export class WordReadComponent{
   @Input() word:string;
   isTranslate:boolean=false;
   wordTranslate:string;
+  wordTranslates:string[];
+
+  private isModalDialogVisible: boolean = false;
+
 
   constructor(private translateService: TranslateService){}
 
   translate(){
+
     if(!this.isTranslate){
-      this.translateService.translated(this.word).then(x=>this.wordTranslate=x);
+      this.translateService.translate(this.word).then(x=>this.wordTranslate=x[0].replace(/ /g,'_'));
+    }else {
+      this.showDialog();
     }
-    this.isTranslate=!this.isTranslate;
+    this.isTranslate=true;
+
   }
 
+
+
+  public showDialog() {
+    this.translateService.translate(this.word, true).then(x=>{
+      this.wordTranslates=[];
+      if(x[0].text!=null){
+        x.forEach((translate)=> this.wordTranslates.push(translate.text))
+      }
+      else {
+        this.wordTranslates=x;
+      }
+    });
+    this.isModalDialogVisible = true;
+  }
+
+  public closeModal(translate:string) {
+    this.isModalDialogVisible = false;
+    this.wordTranslate=translate.replace(/ /g,'_');
+  }
 }
--- src/app/shared/translate.service.ts
+++ src/app/shared/translate.service.ts
@@ -6,39 +6,47 @@ import 'rxjs/add/operator/toPromise'
 @Injectable()
 export class TranslateService{
 
-  token= 'trnsl.1.1.20170122T140228Z.315d034ad2b621da.dcbade2e787cec10b69e4788d710bdf2f5172f82';
-  uri='https://translate.yandex.net/api/v1.5/tr.json/translate?';
+  private TransalteToken= 'trnsl.1.1.20170122T140228Z.315d034ad2b621da.dcbade2e787cec10b69e4788d710bdf2f5172f82';
+  private TranslateUri='https://translate.yandex.net/api/v1.5/tr.json/translate?';
+
+  private DictionaryToken='dict.1.1.20170305T130201Z.2554c112ec72330d.d69305e3c1764a48a7f41ff1e996b392f0fbce55';
+  private DictionaryUri='https://dictionary.yandex.net/api/v1/dicservice.json/lookup?';
 
   constructor(private http:Http){
 
   }
 
-  public translated(text: string) {
-    text=this.remove(text);
-    let result=this.cashe(text);
-    if(result==null){
-      let url =`${this.uri}key=${this.token}&text=${text}&lang=en-ru`;
-      return this.http.get(url)
-        .toPromise()
-        .then(x =>x.json().text[0].replace(/ /g,'_'))
-        .then(x=>this.cashe(text, x));
+  public translate(text:string, isDictionary=false){
+    if(isDictionary){
+      return this._dictionary(text);
     }
     else {
-      return new Promise((resolve)=>resolve(result))
+      return this._translate(text);
     }
   }
 
-  private remove(text:string):string{
-    return text.replace(/['".,?!]/g,'')
+  private _translate(text:string){
+    let url=`${this.TranslateUri}key=${this.TransalteToken}&text=${text}&lang=en-ru`;
+    return this.http.get(url)
+      .toPromise()
+      .then(x=>x.json().text)
   }
 
-  private cashe(text:string, result?:string){
-    if(result==null){
-      result = localStorage.getItem(text);
-    }else{
-      localStorage.setItem(text,result);
-    }
-    return result;
+  private _dictionary(text:string){
+    let url=`${this.DictionaryUri}key=${this.DictionaryToken}&text=${text}&lang=en-ru`;
+    return this.http.get(url)
+      .toPromise()
+      .then(x=>x.json().def)
+      .then(x=>{
+        if(x.length>0){
+          let y=[];
+          x.forEach((items)=>items.tr.forEach((item)=>y.push(item)));
+          return y
+        }
+        else {
+          return this._translate(text)
+        }
+      });
   }
 
 }
