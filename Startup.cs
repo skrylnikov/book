92f58a9ddfd6eefc583a1b40bf0450350ba1931f
worldxaker <djxakir714@gmail.com>
2017-03-09 05:55:23 +0300

    create dotnet core webapi

---
+++ Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace book
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            // Configure Routing
+
+            // Route requests to root for client side routing
+            app.Use(async (context, next) =>
+            {
+              await next();
+              if (context.Response.StatusCode == 404)
+              {
+                context.Request.Path = "/";
+                await next();
+              }
+            });
+
+            app.UseFileServer(enableDirectoryBrowsing: false);
+
+            app.UseMvc(routes =>
+            {
+              // Matches requests that correspond to an existent controller/action pair
+              routes.MapRoute(
+                name: "api",
+                template: "api/{controller=Version}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
